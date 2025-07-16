4dbd0e93ddac7ea9b01a239bee799a9170e1b31d
MR-1985 <marco_r1985@yahoo.de>
2025-07-16 07:38:11 +0200

    feat: added confetti, when number is right / style: more functions, for better readability; new css file; footer added

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 let numberToGues = Math.round(Math.random() * 10);
 let trysCount = 0;
-// let trysRef = document.getElementById("trys");
 let inputField = document.getElementById("input");
 let allreadyUsedNumbers = [];
-createMessageArea();
-createLoadingSpinner();
-createCongratulatinContainer()
+
+function init() {
+    createMessageArea();
+    createLoadingSpinner();
+    createCongratulatinContainer()
+}
+
 
 function checkNumber() {
     showLoadingSpinner();
     setTimeout(() => {
         if (parseInt(inputField.value) !== numberToGues) {
-            allreadyUsedNumbers.push(inputField.value);
             wrongNumber();
+            hideLoadingSpinner();
         } else {
+            hideLoadingSpinner();
             showCongratulation();
             setTimeout(() => {
                 rightNumber();
                 hideCongratulation();
-            }, 500)
+            }, 200)
         }
-        hideLoadingSpinner();
     }, 500);
 }
 
@@ -37,7 +40,7 @@ function createLoadingSpinner() {
     document.getElementById("equal").appendChild(loadingSpinner);
 }
 
-function createCongratulatinContainer(){
+function createCongratulatinContainer() {
     const congratulation = document.createElement("div");
     congratulation.id = "congratulation"
     congratulation.classList.add("dNone");
@@ -54,28 +57,55 @@ function hideLoadingSpinner() {
     messageArea.classList.remove("dNone");
 }
 
-
 function wrongNumber() {
     document.getElementById("messageArea").innerHTML = "Leider war deine zahl falsch, versuche es noch einmal";
+    joinAllreadyUsedNumbers();
     trysCount++;
-    inputField.value = "";
     document.getElementById("trys").innerHTML = "Versuche: " + trysCount;
-    document.getElementById("allreadyTryed").innerHTML = "Bereits versuchte Zahlen: <br>" + allreadyUsedNumbers.join(", ");
+    inputField.value = "";
 }
 
 function rightNumber() {
     trysCount++;
+    joinAllreadyUsedNumbers();
+    throwConfetti();
     document.getElementById("messageArea").innerHTML = "Sehr gut, du hast die Zahl beim " + `${trysCount}` + ". Versuch erraten!"
     document.getElementById("trys").innerHTML = "Versuche: " + trysCount
     inputField.value = "";
 }
 
-function showCongratulation(){
-document.getElementById("congratulation").classList.remove("dNone");
+function pushAllreadyUsedNumbersToArray() {
+    allreadyUsedNumbers.push(inputField.value);
+}
+
+function joinAllreadyUsedNumbers() {
+    pushAllreadyUsedNumbersToArray();
+    document.getElementById("allreadyTryed").innerHTML = "";
+    document.getElementById("allreadyTryed").innerHTML = "Bereits versuchte Zahlen: <br>" + allreadyUsedNumbers.join(",");
+}
+
+function showCongratulation() {
+    hideLoadingSpinner();
+    document.getElementById("congratulation").classList.remove("dNone");
     messageArea.classList.add("dNone");
 };
 
-function hideCongratulation(){
-document.getElementById("congratulation").classList.add("dNone");
+function hideCongratulation() {
+    document.getElementById("congratulation").classList.add("dNone");
     messageArea.classList.remove("dNone");
+}
+
+function throwConfetti() {
+    confetti({
+        angle: 60,
+        spread: 55,
+        particleCount: 50,
+        origin: { x: 0 }
+    });
+    confetti({
+        angle: 120,
+        spread: 55,
+        particleCount: 50,
+        origin: { x: 1 }
+    });
 }
